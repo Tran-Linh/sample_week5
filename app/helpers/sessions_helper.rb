e491a0355a63a36bd0c136544b4bfe391aa65299
Tran-Linh <linh.haenie.bk@gmail.com>
2015-11-14 19:03:45 +0700

    tuan7

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,3 +1,4 @@
+
 module SessionsHelper
 	def log_in(user)
 		session[:user_id] = user.id
@@ -25,9 +26,17 @@ module SessionsHelper
 	end
 
 	def remember(user)
-		
+		user.remember
 		cookies.permanent.signed[:user_id] = user.id
-		
+		cookies.permanent[:remember_token] = user.remember_token
+	end
+
+	def redirect_back_or(default)
+		redirect_to(session[:forwarding_url] || default)
+		session.delete(:forwarding_url)
+	end
+
+	def store_location
+		session[:forwarding_url] = request.url if request.get?
 	end
 end
-
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
+
 class User < ActiveRecord::Base
 	before_save { self.email = email.downcase}
 	validates :name, presence: true, length: { maximum: 50}
@@ -6,7 +7,7 @@ class User < ActiveRecord::Base
 	format: { with: VALID_EMAIL_REGEX},
 	uniqueness: {case_sensitive: false}
 	has_secure_password
-	validates :password, length: { minimum: 6}
+	validates :password, length: { minimum: 6}, allow_blank: true
 
 	def User.digest(string)
 		cost = ActiveModel::SecurePasswod.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
